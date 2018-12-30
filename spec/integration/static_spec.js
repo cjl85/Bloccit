1b79bf6e3a0fbcd326f19301330fafbe2ada5d79
CL <cl@CLs-MacBook-Pro.local>
2018-12-30 16:32:18 -0700

    routing chkpoint

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,12 +5,12 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
   describe("GET /", () => {
 
-    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
 
       request.get(base, (err, res, body) => {
+
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
-        done();
       });
     });
   });
