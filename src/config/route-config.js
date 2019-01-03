e6ca4207b92853aca1f9765c74640600264fe2a3
CL <cl@CLs-MacBook-Pro.local>
2019-01-02 17:10:08 -0700

    topic 1 branch

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
+    const topicRoutes = require("../routes/topics");
+
+    app.use(topicRoutes);
     app.use(staticRoutes);
   }
 }
