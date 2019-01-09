937e0cbcb92e8ce3ad7e67113fe16b4c6157e041
CL <cl@CLs-MacBook-Pro.local>
2019-01-09 13:48:29 -0700

    comments updated chkpoint

--- spec/integration/comments_spec.js
+++ spec/integration/comments_spec.js
@@ -210,8 +210,76 @@ describe("routes : comments", () => {
 
      });
 
+     describe("POST /topics/:topicId/posts/:postId/comments/:id/destroy", () => {
+
+       it("should not delete the comment with the associated ID", (done) => {
+         Comment.all()
+         .then((comments) => {
+           const commentCountBeforeDelete = comments.length;
+
+           expect(commentCountBeforeDelete).toBe(1);
+
+           request.post(
+             `${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`,
+             (err, res, body) => {
+             Comment.all()
+             .then((comments) => {
+               expect(err).toBeNull();
+               expect(comments.length).toBe(commentCountBeforeDelete);
+               done();
+             })
+
+           });
+         })
+       });
+     });
+
    }); //end context for signed in user
 
+   describe("admin performing CRUD actions for Comment", () => {
+
+     beforeEach((done) => {    // before each suite in this context
+       request.get({           // mock authentication
+         url: "http://localhost:3000/auth/fake",
+         form: {
+           role: "member",     // mock authenticate as member user
+           userId: this.user.id
+         }
+       },
+         (err, res, body) => {
+           done();
+         }
+      );
+    });
  });
+
+ describe("POST /topics/:topicId/posts/:postId/comments/:id/destroy", () => {
+
+   it("should delete the comment with the associated ID", (done) => {
+     Comment.all()
+     .then((comments) => {
+       const commentCountBeforeDelete = comments.length;
+
+       expect(commentCountBeforeDelete).toBe(1);
+
+       request.post(
+        `${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`,
+         (err, res, body) => {
+         expect(res.statusCode).toBe(302);
+         Comment.all()
+         .then((comments) => {
+           expect(err).toBeNull();
+           expect(comments.length).toBe(commentCountBeforeDelete - 1);
+           done();
+         })
+
+       });
+     })
+
+   });
+
+ });
+
+
 
 });
